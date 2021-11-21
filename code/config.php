4ee5604bcc2eec9f06530902cd70d7a84b2da970
msed <mohsadki@gmail.com>
2021-11-21 19:48:31 +0100

    git

---
+++ code/config.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Config variables:
+ *
+ * Database credentials
+ * Session variables
+ * Email credentials
+ * TimeZone
+ * Debug console (for outputting php errors and debug messages)
+ * Cache number (in case you update the browser resources)
+ */
+
+// Database
+Config::set("initialize_database", false);
+Config::set("CONN_HOST", "localhost");
+Config::set("CONN_USER", "root");
+Config::set("CONN_PASS", "");
+Config::set("CONN_DDBB", "framework");
+
+// Session
+Config::set("session_name", "MY_SESSION");
+Config::set("session_encrypt_method", "aes-256-cbc");
+Config::set("session_iv", "efz6yAXAQwpeSlEy"); // randomly generated in post install script
+Config::set("session_key", "5hdyySjbBuvxeLIiHB4DmO3eU3O4qoUR"); // randomly generated in post install script
+
+// Email
+Config::set("email_host", "***");
+Config::set("email_username", "***");
+Config::set("email_pass", "***");
+Config::set("email_port", "***");
+Config::set("email_from", "***");
+Config::set("email_from_name", "***");
+
+// Timezone
+Config::set("default_time_zone", "Europe/Madrid");
+
+// Debug
+Config::set("debug_console", true);
+Config::set("execution_time", microtime(true));
+Config::set("show_debug_info", false);
+
+// Cache (change it from update css/js/fonts in the client browser)
+Config::set("cache_version", "0001");
+
+// Security
+Config::set("allow_forms_without_csrf_input", true); // Force form to implement csrf inputs
