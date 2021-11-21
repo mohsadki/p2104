9ebe618f259b0dc4852e93d9e7ff6d60013de812
msed <mohsadki@gmail.com>
2021-11-21 20:24:28 +0100

    vendor

---
+++ code/system/composer/vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '0.96',
+    'version' => '0.96.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'bakeiro/light-php-site',
+  ),
+  'versions' => 
+  array (
+    'bakeiro/light-php-site' => 
+    array (
+      'pretty_version' => '0.96',
+      'version' => '0.96.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'filp/whoops' => 
+    array (
+      'pretty_version' => '2.14.4',
+      'version' => '2.14.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f056f1fe935d9ed86e698905a957334029899895',
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.4',
+      'version' => '1.1.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+    ),
+    'sunra/dbug' => 
+    array (
+      'pretty_version' => '0.2.1',
+      'version' => '0.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ecd8a589dc3ee1bba84095828c222ff560bc19e5',
+    ),
+  ),
+);
